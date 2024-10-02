f79110efb115604e5892b8a8528f7671ef90985a
extrasauceontop <msuchemba@gmail.com>
2024-10-01 20:19:37 -0500

    added files

---
+++ commands.py
@@ -0,0 +1,8 @@
+# clones repo for the first time to your machine
+'git clone <repo link here>'
+
+# checks the status of files in a repo
+'git status'
+
+# adds files to git tracking
+'git add <file name> or git add . to track everything at once'
